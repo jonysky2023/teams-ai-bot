c3c07694ac8be4496999c1cc01e5dbee9f4fe026
Joan Serra <122832864+jonysky2023@users.noreply.github.com>
2026-04-16 14:18:17 +0200

    Update workspaces.py

--- api/workspaces.py
+++ api/workspaces.py
@@ -1,23 +1,16 @@
+import os
+
 WORKSPACES = {
     "default": {
         "api_baseurl": os.getenv("API_BASEURL"),
         "api_user": os.getenv("API_USER"),
-        "api_pass": os.getenv("API_PASS"),
-        "enabled_tools": [
-            "get_device_status",
-            "get_service_info",
-            "restart_service"
-        ]
+        "api_pass": os.getenv("API_PASS")
     },
 
     "client_a": {
         "api_baseurl": "https://client-a.api.com",
         "api_user": "user_a",
-        "api_pass": "pass_a",
-        "enabled_tools": [
-            "get_device_status",
-            "get_user_info"
-        ]
+        "api_pass": "pass_a"
     }
 }
 
